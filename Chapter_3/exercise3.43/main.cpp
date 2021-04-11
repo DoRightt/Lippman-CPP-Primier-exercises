6f52e7fc97a78a3e02b32195a27911cceb0dfd30
cosmicintruder <littlegreenbagg@googlemail.com>
2021-04-11 09:55:21 +0300

    ex3.43/44/45

---
+++ Chapter_3/exercise3.43/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::end;
+using std::begin;
+
+int main() {
+    constexpr size_t ia_row = 3;
+    constexpr size_t ia_col = 4;
+    int ia[ia_row][ia_col] = {
+            {0, 1, 2, 3},
+            {4, 5, 6, 7},
+            {8, 9, 10, 11}
+    };
+
+    for (int (&row)[ia_col] : ia) {
+        for (int col : row) {
+            cout << col << ' ';
+        }
+        cout << endl;
+    }
+
+    for (size_t i = 0; i < ia_row; ++i) {
+        for (size_t k = 0; k < ia_col; ++k) {
+            cout << ia[i][k] << ' ';
+        }
+        cout << endl;
+    }
+
+    for (int (*pr)[ia_col] = ia; pr != ia + ia_row; ++pr) {
+        for (int *pc = *pr; pc != *pr + ia_col; ++pc) {
+            cout << *pc << ' ';
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}
---
+++ Chapter_3/exercise3.45/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::end;
+using std::begin;
+
+int main() {
+    constexpr size_t ia_row = 3;
+    constexpr size_t ia_col = 4;
+    int ia[ia_row][ia_col] = {
+            {0, 1, 2, 3},
+            {4, 5, 6, 7},
+            {8, 9, 10, 11}
+    };
+
+    for (auto &row : ia) {
+        for (int col : row) {
+            cout << col << ' ';
+        }
+        cout << endl;
+    }
+
+    for (size_t i = 0; i < ia_row; ++i) {
+        for (size_t k = 0; k < ia_col; ++k) {
+            cout << ia[i][k] << ' ';
+        }
+        cout << endl;
+    }
+
+    for (auto *pr = ia; pr != ia + ia_row; ++pr) {
+        for (auto *pc = *pr; pc != *pr + ia_col; ++pc) {
+            cout << *pc << ' ';
+        }
+        cout << endl;
+    }
+
+
+    return 0;
+}

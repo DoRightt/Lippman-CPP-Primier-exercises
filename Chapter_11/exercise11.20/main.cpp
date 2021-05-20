f90942c11957087dd07c92bac611450b4d31cbe9
cosmicintruder <littlegreenbagg@googlemail.com>
2021-05-20 09:08:22 +0300

    ex11.20/24

---
+++ Chapter_11/exercise11.20/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::end;
+using std::begin;
+using std::set;
+using std::map;
+using std::for_each;
+
+string f(string s) {
+    constexpr char *punct{"\"'`:*-_;,.?!()[]{}"};;
+    for_each(s.begin(), s.end(), [](char &c) { c = tolower(c); });
+    size_t bg = s.find_first_not_of(punct);
+    size_t ed = s.find_last_not_of(punct);
+
+    if (bg == string::npos) {
+        return "";
+    }
+
+    return s.substr(bg, ed - bg + 1);
+}
+
+int main() {
+    map<string, size_t> word_count;
+    set<string> exclude = {"The", "But", "And", "Or", "An", "A"
+                                                            "the", "but", "and", "or", "an", "a"};
+
+    string word;
+
+    while (cin >> word) {
+        auto ret = word_count.insert({f(word), 1});
+
+        if (!ret.second) {
+            ++ret.first->second;
+        }
+    }
+
+    for (auto val : word_count) {
+        cout << val.first << ": " << val.second << endl;
+    }
+
+    return 0;
+}

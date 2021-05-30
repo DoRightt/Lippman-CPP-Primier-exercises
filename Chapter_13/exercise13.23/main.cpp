3f0b2e448e6003cce16d0521e1f04b2ac9afd8a8
cosmicintruder <littlegreenbagg@googlemail.com>
2021-05-30 13:28:04 +0300

    ex13.23

---
+++ Chapter_13/exercise13.23/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+class HasPtr {
+public:
+    HasPtr(const string &s = string()): ps(new string(s)), i(0) { }
+    HasPtr(const HasPtr &obj): ps(new string(*obj.ps)), i(obj.i) { }
+    ~HasPtr();
+    HasPtr& operator=(const HasPtr &rhs);
+
+    const string &get() {
+        return *ps;
+    }
+
+private:
+    string *ps;
+    int i;
+};
+
+HasPtr::~HasPtr() {
+    delete ps;
+}
+
+HasPtr& HasPtr::operator=(const HasPtr &rhs) {
+    auto newp = new string(*rhs.ps);
+    delete ps;
+    ps = newp;
+    i = rhs.i;
+    return *this;
+}
+
+int main() {
+    HasPtr h1 = {"Test"};
+    HasPtr h2 = h1;
+
+    cout << h2.get() << endl;
+
+    return 0;
+}
